08a2fb880d0debfc03c8326c32ab9db799bd26e0
wangtianliang123 <1124493185@qq.com>
2019-09-01 21:48:25 +0800

    呵呵~

--- pages/Trend.js
+++ pages/Trend.js
@@ -9,54 +9,20 @@ import {
   Button
 } from 'react-native';
 import theme from "../redux/reducer/theme";
-import { RichTextEditor, RichTextToolbar } from 'react-native-zss-rich-text-editor';
 
 
 class Trend extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
-    this.getHTML = this.getHTML.bind(this);
-    this.setFocusHandlers = this.setFocusHandlers.bind(this);
+    this.state = {}
   }
-
-  onEditorInitialized() {
-    this.setFocusHandlers();
-    this.getHTML();
-  }
-
-  async getHTML() {
-    const titleHtml = await this.richtext.getTitleHtml();
-    const contentHtml = await this.richtext.getContentHtml();
-    //alert(titleHtml + ' ' + contentHtml)
-  }
-
-  setFocusHandlers() {
-    this.richtext.setTitleFocusHandler(() => {
-      //alert('title focus');
-    });
-    this.richtext.setContentFocusHandler(() => {
-      //alert('content focus');
-    });
-  }
-
 
   render() {
     const { navigation } = this.props;
     return (
       <Fragment>
         <Text>Trend</Text>
-        <RichTextEditor
-          ref={(r) => this.richtext = r}
-          style={styles.richText}
-          initialTitleHTML={'Title!!'}
-          initialContentHTML={'Hello <b>World</b> <p>this is a new paragraph</p> <p>this is another new paragraph</p>'}
-          editorInitializedCallback={() => this.onEditorInitialized()}
-        />
-        <RichTextToolbar
-            getEditor={() => this.richtext}
-          />
       </Fragment>
     );
   }
